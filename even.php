4578f4f4d18b624730b5c961d6b6a49ee7fea25f
shohan777 <shohan.st27@gmail.com>
2015-09-19 08:55:56 +0600

    update even.php

--- even.php
+++ even.php
@@ -1,19 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Sum of even of odd number</title>
+</head>
+<body>
+	<form action="even.php">
+		<input type="text" name = "type">
+		<input type="text" name ="range">
+		<input type="submit" name="submit">
+	</form>
+</body>
+</html>
+
+
 <?php 
-	$t = '';
-	$in = 68;
-	$sum = 0;
-		for($i=1; $i<=$in; $i++) {
-			if($i%2 == 0) {
-				
-				if($i != $in) {
-					$t .= $i.'+';
-					$sum += $i;
-				}else{
-					$t .= $i;
-					$sum +=$i;
+	
+	if (isset($_GET['submit'])) {
+		function check($type, $range) {
+			$total = '';
+			$sum = 0;
+			if($type == 'even') {
+				for($i=1; $i<=$range; $i++) {
+					if($i%2 == 0) {
+						
+						if($i < $range - 1) {
+							$total .= $i.'+';
+							$sum += $i;
+						}else{
+							$total .= $i;
+							$sum +=$i;
+						}
+					}
+				}
+			}elseif($type == 'odd') {
+				for($i=1; $i<=$range; $i++) {
+					if($i%2 != 0) {
+						
+						if($i < $range - 1) {
+							$total .= $i.'+';
+							$sum += $i;
+						}else{
+							$total .= $i;
+							$sum +=$i;
+						}
+					}
 				}
 			}
-		}
-	echo $t . ' = ' . $sum;
 
+			return $total . ' = ' . $sum;
+		}
+		$type = $_GET['type'];
+		$range = $_GET['range'];
+		$show = check($type, $range);
+		echo "<br />";
+		echo $show;
+	}
  ?>
